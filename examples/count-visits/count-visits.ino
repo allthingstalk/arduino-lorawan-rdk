ebfd5d8041b790105dbdab1211ce25d510bf57e0
esthoril <shendrickx@hotmail.com>
2017-07-13 16:35:01 +0200

    update example file location and header

--- examples/count-visits/count-visits.ino
+++ examples/count-visits/count-visits.ino
@@ -1,18 +1,25 @@
+/*    _   _ _ _____ _    _              _____     _ _     ___ ___  _  __
+ *   /_\ | | |_   _| |_ (_)_ _  __ _ __|_   _|_ _| | |__ / __|   \| |/ /
+ *  / _ \| | | | | | ' \| | ' \/ _` (_-< | |/ _` | | / / \__ \ |) | ' <
+ * /_/ \_\_|_| |_| |_||_|_|_||_\__, /__/ |_|\__,_|_|_\_\ |___/___/|_|\_\
+ *                             |___/
+ *
+ * Copyright 2017 AllThingsTalk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*
-  Copyright 2015-2017 AllThingsTalk
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*/  
-
-/****
  *  AllThingsTalk Maker IoT experiment for LoRa
  *  Version 2.0 dd 29/06/2017
  *  Original author: Jan Bogaerts 2015
@@ -37,7 +44,7 @@
  * pushbutton on the device allows you to reset the count when cleaning is
  * done. This can also be seen as validation that the cleaning crew has
  * actually visited the facility.
- **/
+ */
 
 #include <Wire.h>
 #include <ATT_IOT_LoRaWAN.h>
--- examples/guard-your-stuff/guard-your-stuff.ino
+++ examples/guard-your-stuff/guard-your-stuff.ino
@@ -1,18 +1,25 @@
+/*    _   _ _ _____ _    _              _____     _ _     ___ ___  _  __
+ *   /_\ | | |_   _| |_ (_)_ _  __ _ __|_   _|_ _| | |__ / __|   \| |/ /
+ *  / _ \| | | | | | ' \| | ' \/ _` (_-< | |/ _` | | / / \__ \ |) | ' <
+ * /_/ \_\_|_| |_| |_||_|_|_||_\__, /__/ |_|\__,_|_|_\_\ |___/___/|_|\_\
+ *                             |___/
+ *
+ * Copyright 2017 AllThingsTalk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
 /*
-  Copyright 2015-2017 AllThingsTalk
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*/  
-
-/****
  *  AllThingsTalk Maker Cloud IoT experiment for LoRa
  *  Version 1.0 dd 09/11/2015
  *  Original author: Jan Bogaerts 2015
@@ -35,7 +42,7 @@
  * notification which will be visible in the AllthingsTalk Developer
  * Cloud and on our Smartphone. If the lock is turned off, nothing will
  * happen, regardless of any movement of the device.
- **/
+ */
 
 #include <Wire.h>
 #include <SoftwareSerial.h>
